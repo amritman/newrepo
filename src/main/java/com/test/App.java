329e18dcf9bef4f54d1ca576f6f0a6f81769e253
amritman <97688746+amritman@users.noreply.github.com>
2022-03-02 23:42:28 +0545

    Update App.java

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -2,7 +2,7 @@ package com.test;
 
 class App {
 	public static void main(String[] args){
-		System.out.println("hello all Friends");
+		System.out.println("hello all Friends this is Amrit");
 		
 	}
 
