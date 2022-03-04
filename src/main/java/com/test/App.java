cde84a2a7f5df2ead4e5332989c0cc4eda26f6f7
amritman <97688746+amritman@users.noreply.github.com>
2022-03-04 21:37:04 +0545

    Update App.java

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -2,7 +2,7 @@ package com.test;
 
 class App {
 	public static void main(String[] args){
-		System.out.println("hello all Friends this is Amrit providing Devops Class");
+		System.out.println("This is Devops Class");
 		
 	}
 
