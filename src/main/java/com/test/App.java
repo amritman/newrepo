11b67aebcc29c9d6191e0915bdd7766eff93e4ef
amritman <97688746+amritman@users.noreply.github.com>
2024-01-18 08:11:44 +0545

    Update App.java

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -2,7 +2,7 @@ package com.test;
 
 class App {
 	public static void main(String[] args){
-		System.out.println("This is Devops Class Running");
+		SystemHello.out.println("This is Devops Class Running");
 		
 	}
 
